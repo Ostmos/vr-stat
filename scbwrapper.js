14bd60cfdb429fc5d3e4185fe57c87cab6f9f929
O99ie <business99ogge@gmail.com>
2018-01-25 17:31:39 +0100

    staplar och grejer

--- scbwrapper.js
+++ scbwrapper.js
@@ -2,11 +2,11 @@ function getX(){
     return ["2018", "2019", "2020", "2021", "2022"];
 }
 
-function getY() {
-    return [5, 10, 2, 20, 30];
-}
-
 function getZ() {
     return ["Göteborg", "Stockholm", "Luleå", "Malmö", "Växjö"];
+}
+
+function getY() {
+    return [5, 10, 2, 20, 30, 34, 23, 54, 4,23, 2, 325, 10, 2, 20, 30, 34, 23, 54, 4,23, 2, 32, 2, 9];
 }
 
