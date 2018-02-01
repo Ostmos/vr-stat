57b11a637deaf9baf5ff5a51796e6198a38fe700
henbos <henbos@student.chalmers.se>
2018-02-01 22:45:04 +0100

    Code cleanup and scaling works

--- src/components/bar-chart.js
+++ src/components/bar-chart.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent('bar-chart', {
     schema: {
-    size: {type: 'number', default: 10},
-    offset: {type: 'number', default: 10},
+    outerPlaneSize: {type: 'number', default: 10},
+    offset: {type: 'number', default: 1},
     color: {type: 'color', default: '#FFF'}
     },
 
@@ -9,58 +9,68 @@ AFRAME.registerComponent('bar-chart', {
         var data = this.data;
         var object = this.el.object3D
 
-        var geometry = new THREE.PlaneGeometry(data.size, data.size);
-        geometry.rotateX(Math.PI / 2);
-        var material = new THREE.MeshBasicMaterial({color: 0x2A363B, side: THREE.DoubleSide});
-        var plane = new THREE.Mesh(geometry, material);
-        
-        object.add(plane);
+        var innerPlaneSize = data.outerPlaneSize - data.offset * 2;
 
-        const BAR_SIZE = 0.5;
-        const START_X = -4.75;
-        const START_Z = 4.75;
+        // Outer plane
+        var outerPlaneGeometry = new THREE.PlaneGeometry(data.outerPlaneSize, data.outerPlaneSize);
+        outerPlaneGeometry.rotateX(Math.PI / 2);
+        var outerPlaneMaterial = new THREE.MeshBasicMaterial({color: 0x2A363B, side: THREE.DoubleSide});
+        var outerPlane = new THREE.Mesh(outerPlaneGeometry, outerPlaneMaterial);
+        
+        object.add(outerPlane);
+
+        // Test data
+        var heights = new Array(400);
+        for (var i = 0; i < 400; i++) {
+            heights[i] = Math.floor(Math.random() * Math.floor(data.outerPlaneSize));
+        }
 
         // Bars
-        for (var i = 0, j = 0; i < 400; i++) {
-            var color = new THREE.Color(Math.random() * 0xff0000);
-            var material = new THREE.MeshBasicMaterial({color: color});
-            const HEIGHT = Math.floor(Math.random() * Math.floor(data.size / 2));
-            var geometry = new THREE.BoxGeometry(BAR_SIZE, HEIGHT, BAR_SIZE);
-            var cube = new THREE.Mesh(geometry, material);
+        var barWidth = (innerPlaneSize / Math.sqrt(heights.length));
+        var barPos = new THREE.Vector3(
+            -innerPlaneSize / 2 + barWidth / 2,
+            0, 
+            innerPlaneSize / 2 - barWidth / 2,
+        );
 
-            if (i % 20 == 0 && i != 0) {
+        for (var i = 0, j = 0; i < heights.length; i++) {
+            if (i % Math.sqrt(heights.length) == 0 && i != 0) {
                 j++;   
             }
+            if (heights[i] != 0) {
+                var color = new THREE.Color(Math.random() * 0xFF0000);
+                var material = new THREE.MeshBasicMaterial({color: color});
+                var geometry = new THREE.BoxGeometry(barWidth, heights[i], barWidth);
+                var cube = new THREE.Mesh(geometry, material);
 
-            var x = START_X + BAR_SIZE * (i % 20);
-            var z = START_Z - BAR_SIZE * j;
+                cube.translateX(barPos.x + barWidth * (i % 20));
+                cube.translateY(heights[i] / 2);
+                cube.translateZ(barPos.z - barWidth * j);
 
-            cube.translateX(x);
-            cube.translateY(HEIGHT / 2);
-            cube.translateZ(z);
-
-            object.add(cube);
+                object.add(cube);
+            }
         }
-        
 
         // Lines
-        const C = data.size;
+        const lengthToCorners = data.outerPlaneSize / 2;
 
-        var start = new THREE.Vector3(-C / 2, 0, C / 2);
-        var start2 = new THREE.Vector3(-C / 2, 0, -C / 2);
-        var start3 = new THREE.Vector3(C / 2, 0, -C / 2);
+        var corner1 = new THREE.Vector3(-lengthToCorners, 0, lengthToCorners);
+        var corner2 = new THREE.Vector3(-lengthToCorners, 0, -lengthToCorners);
+        var corner3 = new THREE.Vector3(lengthToCorners, 0, -lengthToCorners);
 
         var material = new THREE.LineBasicMaterial({color: 0x2A363B});
         var geometry = new THREE.Geometry();
         
-        for (var i = 0; i < 10; i++) {
-            geometry.vertices.push(new THREE.Vector3(start.x, start.y + i, start.z));
-            geometry.vertices.push(new THREE.Vector3(start2.x, start2.y + i, start2.z));
-            geometry.vertices.push(new THREE.Vector3(start2.x, start2.y + i, start2.z));
-            geometry.vertices.push(new THREE.Vector3(start3.x, start3.y + i, start3.z));
+        const numberOfLines = 10;
+        var lineStep = Math.max(...heights) / numberOfLines;
+
+        for (var i = 1; i <= 10; i++) {
+            geometry.vertices.push(new THREE.Vector3(corner1.x, corner1.y + lineStep * i, corner1.z));
+            geometry.vertices.push(new THREE.Vector3(corner2.x, corner2.y + lineStep * i, corner2.z));
+            geometry.vertices.push(new THREE.Vector3(corner2.x, corner2.y + lineStep * i, corner2.z));
+            geometry.vertices.push(new THREE.Vector3(corner3.x, corner3.y + lineStep * i, corner3.z));
         }
         var line = new THREE.LineSegments( geometry, material );
-
         object.add( line );
     },
 });
