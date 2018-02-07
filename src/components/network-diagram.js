98ba45cf6bebea32b34c6295328037f1bcc5e5ed
henbos <henbos@student.chalmers.se>
2018-02-07 01:36:22 +0100

    Created simple pie chart

--- src/components/network-diagram.js
+++ src/components/network-diagram.js
@@ -1,4 +1,3 @@
-
 AFRAME.registerComponent('network-diagram', {
     schema: {
         size: {type: 'number', default: 1},
@@ -15,12 +14,18 @@ AFRAME.registerComponent('network-diagram', {
         var material = new THREE.MeshBasicMaterial({color: 0xfe4365});
         var mesh = new THREE.Mesh(geometry, material );
         var mesh2 = new THREE.Mesh(geometry, material );
+        var mesh3 = new THREE.Mesh(geometry, material );
+        var mesh4 = new THREE.Mesh(geometry, material );
 
         mesh2.translateX(-10);
         mesh2.translateY(-10);
+        mesh3.translateZ(-10);
+        mesh4.translateY(10);
 
         object.add(mesh);
         object.add(mesh2);
+        object.add(mesh3);
+        object.add(mesh4);
 
         // Lines
         var material = new THREE.LineBasicMaterial({color: 0x2A363B});
@@ -28,6 +33,10 @@ AFRAME.registerComponent('network-diagram', {
         
         geometry.vertices.push(new THREE.Vector3(0, 0, 0));
         geometry.vertices.push(new THREE.Vector3(-10, -10, 0));
+        geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+        geometry.vertices.push(new THREE.Vector3(0, 0, -10));
+        geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+        geometry.vertices.push(new THREE.Vector3(0, 10, 0));
 
         var line = new THREE.LineSegments( geometry, material );
 
