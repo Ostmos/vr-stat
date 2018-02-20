e81dfb2822447a5b8738af9375e95216bac94c2e
Henrik <henbos@student.chalmers.se>
2018-02-20 19:45:51 +0100

    Test and controller

--- src/components/controllers/controller-listener.js
+++ src/components/controllers/controller-listener.js
@@ -3,12 +3,14 @@ AFRAME.registerComponent('controller-listener', {
     init: function() {
         var el = this.el;
 
-        el.addEventListener('hover-start', function() {
+        el.addEventListener('hover-start', function(evt) {
+            console.log(evt);
             var children = el.childNodes;
             children[0].setAttribute("visible", "true");
         });
 
-        el.addEventListener('hover-end', function() {
+        el.addEventListener('hover-end', function(evt) {
+            console.log(evt);
             var children = el.childNodes;
             children[0].setAttribute("visible", "false");
         });
