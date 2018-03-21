739bc85fdd0520285bc5bc70dc576bf442ec5570
JacobJNilsson <jacobjnilsson@gmail.com>
2018-03-21 17:20:43 +0100

    basic functionality for new scatterplot

--- src/components/scatter-plot-3.js
+++ src/components/scatter-plot-3.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent('scatter-plot-3', {
     schema: {
         size: { type: 'number', default: 3 },
-        radius: { type: 'number', default: 0.02 },
+        nodeSize: { type: 'number', default: 0.1 },
         color: { type: 'color', default: '#FFF' },
         title: {type: 'string', default: 'no name'}
     },
@@ -9,14 +9,14 @@ AFRAME.registerComponent('scatter-plot-3', {
         const twitterData = JSON.parse(`{
             "stats": {
                 "labels": {"x": "Retweet", "y": "Favorite", "z": "Number of tweets"},
-                "maxValues": {"x": 4, "y": 3, "z": 651},
+                "maxValues": {"x": 4, "y": 3, "z": 342},
                 "minValues": {"x": 0, "y": 0, "z": 19},
                 "values": [
-                    {"x": 3, "y": 2, "z": 342},
+                    {"x": 4, "y": 3, "z": 342},
                     {"x": 0, "y": 0, "z": 50},
                     {"x": 0, "y": 1, "z": 100},
                     {"x": 0, "y": 2, "z": 23},
-                    {"x": 1, "y": 0, "z": 651},
+                    {"x": 1, "y": 0, "z": 251},
                     {"x": 1, "y": 1, "z": 19},
                     {"x": 1, "y": 2, "z": 100},
                     {"x": 2, "y": 0, "z": 123},
@@ -37,9 +37,11 @@ AFRAME.registerComponent('scatter-plot-3', {
             this.data.size,
             this.data.size
         );
-        let planeMaterial = new THREE.MeshBasicMaterial({color: "red", side: THREE.FrontSide});
+        let planeMaterial = new THREE.MeshBasicMaterial({color: "red", side: THREE.DoubleSide});
         let planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
         planeMesh.rotation.x = -Math.PI / 2;
+        let vector = new THREE.Vector3(this.data.size / 2, 0, this.data.size / 2);
+        this.setPosition(planeMesh, vector);
         this.el.setObject3D("base", planeMesh);
     },
     createWalls: function(){
@@ -47,42 +49,60 @@ AFRAME.registerComponent('scatter-plot-3', {
             this.data.size,
             this.data.size
         );
-        let planeMaterial_1 = new THREE.MeshBasicMaterial({color: "green", side: THREE.FrontSide});
+        let planeMaterial_1 = new THREE.MeshBasicMaterial({color: "green", side: THREE.DoubleSide});
         let planeMesh_1 = new THREE.Mesh(planeGeometry, planeMaterial_1);
-        planeMesh_1.rotation.y = -Math.PI / 2;
+        planeMesh_1.rotation.y = Math.PI / 2;
+        let vector_1 = new THREE.Vector3(0, this.data.size / 2, this.data.size / 2);
+        this.setPosition(planeMesh_1, vector_1);
         this.el.setObject3D("wall_1", planeMesh_1);
 
-        let planeMaterial_2 = new THREE.MeshBasicMaterial({color: "yellow", side: THREE.FrontSide});
+        let planeMaterial_2 = new THREE.MeshBasicMaterial({color: "yellow", side: THREE.DoubleSide});
         let planeMesh_2 = new THREE.Mesh(planeGeometry, planeMaterial_2);
+        let vector_2 = new THREE.Vector3(this.data.size / 2, this.data.size / 2, 0);
+        this.setPosition(planeMesh_2, vector_2);
         this.el.setObject3D("wall_2", planeMesh_2);
     },
     createNodes: function(stats){
-        let i = 0;
-        let node = stats.values[i];
-        let x = node.x;
-        let y = node.y;
-        let z = node.z;
+        
+        for(let index in stats.values){
+            //load values
+            let node = stats.values[index];
+            let x = node.x;
+            let y = node.y;
+            let z = node.z;
 
-        //Scale
-        let maxValues = stats.MaxValues;
-        x = (x / maxValues.x) * this.data.size; 
-        y = (y / maxValues.y) * this.data.size;
-        z = (z / maxValues.z) * this.data.size;
-        let vector = new THREE.Vector3(x, y, z)
-        this.createNode(vector);
+            //Scale
+            let max = stats.maxValues;
+            let min = stats.minValues;
+            let maxX = max.x-min.x;
+            let maxY = max.y-min.y;
+            let maxZ = max.z-min.z;
+            x = ((x - min.x) / maxX) * this.data.size; 
+            y = ((y - min.y) / maxY) * this.data.size;
+            z = ((z - min.z) / maxZ) * this.data.size;
+            let vector = new THREE.Vector3(x, y, z)
+            console.log("x: " + x + "\ny: " + y + "\nz: " + z);
+            //create node
+            this.createNode(vector, index);
+        }
+        let vector_ = new THREE.Vector3(3,3,3);
+        this.createNode(vector_, 99);
     },
-    createNode: function(vector){
+    createNode: function(vector, index){
         let planeGeometry = new THREE.PlaneGeometry(
-            0.02,
-            0,02
+            this.data.nodeSize,
+            this.data.nodeSize
         );
-        let planeMaterial = new THREE.MeshBasicMaterial({color: "blue", side: THREE.doubleside});
+        let planeMaterial = new THREE.MeshBasicMaterial({color: "blue", side: THREE.DoubleSide});
         let planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
-        mesh.position.set(
-            vector.x,
-            vector.y,
-            vector.z
-        )
-        this.el.setObject3D("node", planeMesh);
+        this.setPosition(planeMesh, vector);
+        this.el.setObject3D("node_"+index, planeMesh);
+    },
+    setPosition: function(obj, vector){
+        let size = this.data.size;
+
+        obj.position.y = vector.y - size / 2;
+        obj.position.z = vector.z - size / 2;
+        obj.position.x = vector.x - size / 2;
     } 
 })
