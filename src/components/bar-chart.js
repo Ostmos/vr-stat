db01bff27d225b0500dcc49675ebc0cb4cb99170
henbos <henbos@student.chalmers.se>
2018-03-22 12:23:08 +0100

    Added title

--- src/components/bar-chart.js
+++ src/components/bar-chart.js
@@ -13,8 +13,10 @@ AFRAME.registerComponent("bar-chart", {
         title: {type: "string", default: ""},
         xLabel: {type: "string", default: ""},
         yLabel: {type: "string", default: ""},
+        suffix: {type: "string", default: ""},
 
-        scale: {type: "number", default: 1},
+        yScale: {type: "number", default: 1.0},
+        scale: {type: "number", default: 1.0},
         barWidth: {type: "number", default: 0.3},
         barPadding: {type: "number", default: 0.1},
         fontSize: {type: "number", default: 2}
@@ -28,6 +30,9 @@ AFRAME.registerComponent("bar-chart", {
         .then(function(jsonData){
             self.createChart(jsonData);
         });
+
+        let scale = this.data.scale;
+        this.el.object3D.scale.set(scale, scale, scale);
     },
 
     createChart: function(bars) {
@@ -46,34 +51,21 @@ AFRAME.registerComponent("bar-chart", {
         const BAR_TOTAL_SIZE = data.barWidth + data.barPadding * 2;
         const PANEL_WIDTH = bars.length * BAR_TOTAL_SIZE;
         const PANEL_DEPTH = BAR_TOTAL_SIZE;
+        const PANEL_START_POS = -PANEL_WIDTH / 2 + BAR_TOTAL_SIZE / 2; 
         const PLANE_GEOMETRY = new THREE.PlaneGeometry(PANEL_WIDTH, PANEL_DEPTH);
         const PLANE_MATERIAL = new THREE.MeshBasicMaterial({color: 0xACDBC9, side: THREE.DoubleSide});
         let planeMesh = new THREE.Mesh(PLANE_GEOMETRY, PLANE_MATERIAL);
         planeMesh.rotation.x = Math.PI / 2;
         this.el.setObject3D("planeMesh", planeMesh);
-        
-        // Bars
-        const PANEL_START_POS = -PANEL_WIDTH / 2 + BAR_TOTAL_SIZE / 2; 
-        for(let i = 0; i < bars.length; i++){
-            const BAR_GEOMETRY = new THREE.BoxBufferGeometry(data.barWidth, bars[i].y * data.scale, data.barWidth);
-            const BAR_MATERIAL = new THREE.MeshStandardMaterial({color: 0xF48B94});
-            let barMesh = new THREE.Mesh(BAR_GEOMETRY, BAR_MATERIAL);
-            barMesh.position.set(PANEL_START_POS + BAR_TOTAL_SIZE * i, bars[i].y / 2 * data.scale, 0);
-            this.el.setObject3D("bar" + i, barMesh);
-        }
 
-        this.createLabels(xLabels, yValues, BAR_TOTAL_SIZE, PANEL_WIDTH);
-    },
-
-    createLabels: function(xLabels, yValues, barSize, planeWidth, bars) {
-        // Font
+        // Load texture
         var self = this;
         var textureLoader = new THREE.TextureLoader();
         textureLoader.load('./src/assets/fonts/dejavu/DejaVu-sdf.png', function (texture) {
             texture.needsUpdate = true;
             texture.anisotropy = 16;
 
-            var material = new THREE.RawShaderMaterial(SDFShader({
+            var fontTexture = new THREE.RawShaderMaterial(SDFShader({
                 map: texture,
                 side: THREE.DoubleSide,
                 transparent: true,
@@ -81,29 +73,75 @@ AFRAME.registerComponent("bar-chart", {
             }))
             
             fontLoader('./src/assets/fonts/dejavu/DejaVu-sdf.fnt', function(err, font) {
-                const TEXT_WIDTH = 300;
-
-                // X labels
-                for (let i = 0; i < xLabels.length; i++) {
-                    var geometry = fontCreator({
-                        width: TEXT_WIDTH,
-                        font: font,
-                        letterSpacing: 1,
-                        align: "right",
-                        text: xLabels[i]
-                    })
-
-                    var mesh = new THREE.Mesh(geometry, material);
-                    var textAnchor = new THREE.Object3D();
-                    textAnchor.scale.multiplyScalar(0.004);
-                    // 1.2 is magic, should fix that
-                    textAnchor.position.set((-planeWidth / 2 + barSize / 2) + barSize * i, 0, barSize / 2 + 1.2);
-                    textAnchor.rotation.set(Math.PI / 2, 0, -Math.PI / 2);
-                    textAnchor.add(mesh);
-                    self.el.setObject3D('mesh' + i, textAnchor);
-                }
+                self.createBars(yValues, PANEL_START_POS, BAR_TOTAL_SIZE, PANEL_WIDTH, font, fontTexture);
+                self.createLabels(xLabels, BAR_TOTAL_SIZE, PANEL_WIDTH, font, fontTexture);
+                self.createTitle(Math.max(...yValues), PANEL_WIDTH, font, fontTexture);
             });
         })
+        
+    },
+
+    createBars: function(yValues, startPos, totalBarSize, panelWidth, font, fontTexture){
+        let data = this.data;
+
+        for(let i = 0; i < yValues.length; i++){
+            const BAR_GEOMETRY = new THREE.BoxBufferGeometry(data.barWidth, yValues[i] * data.yScale, data.barWidth);
+            const BAR_MATERIAL = new THREE.MeshStandardMaterial({color: 0xF48B94});
+            let barMesh = new THREE.Mesh(BAR_GEOMETRY, BAR_MATERIAL);
+            barMesh.position.set(startPos + totalBarSize * i, yValues[i] / 2 * data.yScale, 0);
+            this.el.setObject3D("bars" + i, barMesh);
+
+            var fontGeometry = fontCreator({
+                width: 200,
+                font: font,
+                letterSpacing: 1,
+                align: "center",
+                text: yValues[i] + data.suffix
+            })
+
+            var mesh = new THREE.Mesh(fontGeometry, fontTexture);
+            mesh.rotation.set(Math.PI, 0, 0);
+            mesh.scale.multiplyScalar(0.004);
+            mesh.position.set(-totalBarSize / 1.3, yValues[i] / 2 * data.yScale + 0.05, 0);
+            barMesh.add(mesh);
+        }
+    },
+
+    createLabels: function(xLabels, barSize, planeWidth, font, fontTexture) {
+        for (let i = 0; i < xLabels.length; i++) {
+            var fontGeometry = fontCreator({
+                width: 300,
+                font: font,
+                letterSpacing: 1,
+                align: "right",
+                text: xLabels[i]
+            })
+
+            var mesh = new THREE.Mesh(fontGeometry, fontTexture);
+            var textAnchor = new THREE.Object3D();
+            textAnchor.scale.multiplyScalar(0.004);
+            // 1.2 is magic, should fix that
+            textAnchor.position.set((-planeWidth / 2 + barSize / 2) + barSize * i, 0, barSize / 2 + 1.2);
+            textAnchor.rotation.set(Math.PI / 2, 0, -Math.PI / 2);
+            textAnchor.add(mesh);
+            this.el.setObject3D('xLabel' + i, textAnchor);
+        }
+    },
+
+    createTitle: function(yMax, panelWidth, font, fontTexture) {
+        var fontGeometry = fontCreator({
+            width: 1000,
+            font: font,
+            letterSpacing: 1,
+            align: "left",
+            text: this.data.title
+        })
+        var mesh = new THREE.Mesh(fontGeometry, fontTexture);
+        mesh.rotation.set(Math.PI, 0, 0);
+        mesh.scale.multiplyScalar(0.006);
+        const TITLE_OFFSET = 0.5;
+        mesh.position.set(-panelWidth / 2, yMax * this.data.yScale + TITLE_OFFSET, 0);
+        this.el.setObject3D('title', mesh);
     },
 
 
