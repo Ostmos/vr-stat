e10eca407a92d1192ead35c4803abf218b24d105
Henrik <henbos@student.chalmers.se>
2018-03-23 21:04:27 +0100

    Ray caster fix: use master branch AFrame, teleporter fix: camera rig

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // External components
 require("super-hands");
+require("aframe-teleport-controls");
 
 // Components
+require("./components/oculus-controller-listener");
 require("./components/bar-chart");
